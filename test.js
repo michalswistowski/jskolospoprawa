5d601fc5fe5e95f7a214cc3f3c1b502ccfe96c64
Maciej Słupianek <101468252+M-ac-i-ej-s@users.noreply.github.com>
2022-07-06 13:30:54 +0200

    Update test.js

--- test.js
+++ test.js
@@ -26,10 +26,10 @@ class Grid {
         const types = [ "LAND", "WATER", "ICE"];
         const itemType = types[Math.floor(Math.random()*types.length)];
         for(let x = 0 ; x < this.width ; x++){
-            if(this.grid === null) grid = []
+            if(this.grid === null) this.grid = []
             this.grid.push([])
             for(let y = 0 ; y < this.height ; y++){
-                this.grid.push(Tile(i,y,itemType))
+                this.grid.push(new Tile(x,y,itemType))
             }
         }
     }
@@ -39,13 +39,13 @@ class Grid {
         const types = [ "LAND", "WATER", "ICE"];
         const itemType = types[Math.floor(Math.random()*types.length)];
         for(let x = 0 ; x < this.width ; x++){
-            if(this.grid === null) grid = []
+            if(this.grid === null) this.grid = []
             this.grid.push([])
             for(let y = 0 ; y < this.height ; y++){
                 for(let z = 0 ; z < predefinedTiles.length ; z++){
                     if(predefinedTiles[z].x == x && predefinedTiles[z].y == y) this.grid.push(predefinedTiles[z]) 
                 }
-                if(this.grid[x][y] === undefined) this.grid.push(Tile(i,y,itemType))
+                if(this.grid[x][y] === undefined) this.grid.push(new Tile(x,y,itemType))
             }
         }
     }
@@ -81,14 +81,26 @@ class Ship {
         }
     }
     sail(){
-        if(this.grid[x-1][y].type === "LAND" && this.grid[x-1][y].type === "ICE") return "nie możesz tędy płynąć"
-        if(this.grid[x+1][y].type === "LAND" && this.grid[x-1][y].type === "ICE") return "nie możesz tędy płynąć"
-        if(this.grid[x][y-1].type === "LAND" && this.grid[x-1][y].type === "ICE") return "nie możesz tędy płynąć"
-        if(this.grid[x][y+1].type === "LAND" && this.grid[x-1][y].type === "ICE") return "nie możesz tędy płynąć"
-        if(this.direction === "N") this.x-=1
-        if(this.direction === "S") this.x+=1
-        if(this.direction === "E") this.y+=1
-        if(this.direction === "W") this.y-=1
+        if(this.direction === "N" && this.grid[this.x-1][this.y].type !== "LAND" && this.grid[this.x-1][this.y].type !== "ICE") {
+            this.x-=1
+        } else {
+            return "nie możesz tędy płynąć"
+        }
+        if(this.direction === "S" && this.grid[this.x+1][this.y].type !== "LAND" && this.grid[this.x-1][this.y].type !== "ICE") {
+            this.x+=1
+        } else {
+            return "nie możesz tędy płynąć"
+        }
+        if(this.direction === "E" && this.grid[this.x][this.y-1].type !== "LAND" && this.grid[this.x-1][this.y].type !== "ICE") {
+            this.y+=1
+        } else {
+            return "nie możesz tędy płynąć"
+        }
+        if(this.direction === "W" && this.grid[this.x][this.y+1].type !== "LAND" && this.grid[this.x-1][this.y].type !== "ICE") {
+            this.y-=1
+        } else {
+            return "nie możesz tędy płynąć"
+        }
     }
 
 }
@@ -104,22 +116,22 @@ class IcebreakerShip extends Ship {
         if(this.direction === "W") this.direction = "E"
     }
     sail(){
-        if(this.direction === "N" && this.grid[x-1][y].type !== "LAND") {
+        if(this.direction === "N" && this.grid[this.x-1][this.y].type !== "LAND") {
             this.x-=1
         } else {
             Ship.turn("P")
         }
-        if(this.direction === "S" && this.grid[x+1][y].type !== "LAND") {
+        if(this.direction === "S" && this.grid[this.x+1][this.y].type !== "LAND") {
             this.x+=1
         } else {
             Ship.turn("P")
         }
-        if(this.direction === "E" && this.grid[x1][y-1].type !== "LAND") {
+        if(this.direction === "E" && this.grid[this.x][this.y-1].type !== "LAND") {
             this.y+=1
         } else {
             Ship.turn("P")
         }
-        if(this.direction === "W" && this.grid[x][y+1].type !== "LAND") {
+        if(this.direction === "W" && this.grid[this.x][this.y+1].type !== "LAND") {
             this.y-=1
         } else {
             Ship.turn("P")
@@ -127,4 +139,33 @@ class IcebreakerShip extends Ship {
     }
 }
 
+const grid = new Grid();
+grid.generate(10, 10); 
+console.log(grid.getTile(2,2)) // 2,2 LAND
+grid.generate(10, 5); 
+
+const predefTiles = [
+    new Tile(1, 2, "LAND"),
+    new Tile(2, 2, "WATER"),
+    new Tile(5, 5, "ICE")
+];
+
+grid.generate(10, 10, predefTiles);
+console.log(grid.getTile(2,2)) // 2,2 WATER
+
+const ship = new Ship(2, 4, "N", grid);
+const ship2 = new Ship(1,2, "N", grid); // błąd
+const icebreaker = new IcebreakerShip(3,5, grid);
+
+ship.turn("P"); // Statek obrócony o 90 st. w prawo
+ship.sail(); // Statek poruszyl się o 1 kafele na wschód
+ship.sail(); // Statek napotkał ląd, pozostał w miejscu
+ship.turn("L")  // Statek obrócony o 90 st. w lewo
+ship.sail(); // Statek napotkał inny statek, pozostał w miejscu
+
+icebreaker.sail(); // Statek napotkał lądolód, przedarł się przez niego
+console.log(icebreaker.getShipInfo()); // "N", 3,6
+
 wez ten kod z dolu polecenia sprawdz bo pelno bledow
+
+dalej sa bledy ale mniej
